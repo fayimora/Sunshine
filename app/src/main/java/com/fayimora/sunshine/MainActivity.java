c818ae31811db43b084c416004abda245ed0c496
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-08-03 10:35:17 +0100

    Remove unused imports

--- app/src/main/java/com/fayimora/sunshine/MainActivity.java
+++ app/src/main/java/com/fayimora/sunshine/MainActivity.java
@@ -1,7 +1,6 @@
 package com.fayimora.sunshine;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,10 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
